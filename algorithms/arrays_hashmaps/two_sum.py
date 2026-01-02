cf47def6144ff82f85e9c2ecdb14079f340858f9
Henry <henry@Henrys-MacBook-Air.local>
2026-01-02 14:45:19 -0500

    initial commit

---
+++ algorithms/arrays_hashmaps/two_sum.py
@@ -0,0 +1,9 @@
+"""
+Two Sum
+
+Given an array of integers and a target value, return the indices
+of the two numbers that add up to the target.
+
+This implementation prioritizes clarity, correctness, and
+explainability over clever tricks.
+"""
