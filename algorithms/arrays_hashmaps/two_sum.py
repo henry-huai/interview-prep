3889163972ef49395b7adc3149825c97e284251b
Henry <henry@Henrys-MacBook-Air.local>
2026-01-11 18:07:51 -0500

    two sum solution

--- algorithms/arrays_hashmaps/two_sum.py
+++ algorithms/arrays_hashmaps/two_sum.py
@@ -7,3 +7,17 @@ of the two numbers that add up to the target.
 This implementation prioritizes clarity, correctness, and
 explainability over clever tricks.
 """
+
+"""
+Time complexity: O(n^2)
+
+Space complexity: O(n) (peak extra memory due to the largest slice)
+"""
+def two_sum(numbers : list[int], target: int) -> bool:
+    for i in range(len(numbers)):
+        remaining = target - numbers[i]
+        if remaining in numbers[i+1:]:
+            return True
+    return False
+
+
